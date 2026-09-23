8f2e80d85deed33a80546dd31c8a31c8fc8fa266
signatureproprt-boop <signatureproprt@gmail.com>
2026-09-23 17:47:24 +0530

    test: use node syntax checker for clients workspace script

--- full-system/crm/test/clientWorkspaceHubSyntax.test.js
+++ full-system/crm/test/clientWorkspaceHubSyntax.test.js
@@ -1,11 +1,12 @@
 const test = require('node:test');
 const assert = require('node:assert/strict');
-const fs = require('node:fs');
+const { execFileSync } = require('node:child_process');
 const path = require('node:path');
 
 test('clients workspace browser script parses as JavaScript', () => {
   const file = path.join(__dirname, '..', 'client-workspace-hub.js');
-  const source = fs.readFileSync(file, 'utf8');
-  assert.ok(source.length > 1000, 'clients workspace script is unexpectedly small');
-  assert.doesNotThrow(() => new Function(source), 'clients workspace script must parse');
+  assert.doesNotThrow(
+    () => execFileSync(process.execPath, ['--check', file], { encoding: 'utf8', stdio: 'pipe' }),
+    'clients workspace script must parse'
+  );
 });
